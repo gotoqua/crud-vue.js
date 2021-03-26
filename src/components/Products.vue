e2120b63720660a5701f65e401630d06aa8af38b
gotoqua <caiquech7@gmail.com>
2021-03-26 02:27:02 -0300

    Primeiras construções

---
+++ src/components/Products.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h1>Produtos</h1>
+        <hr>
+
+        <div class="form-todo form-group">
+
+
+
+        </div>
+
+        <hr>
+        <div class="form-todo form-group">
+            <p>
+                <input type="text" placeholder="Nome" name="nome" class="form-control" >
+            </p>
+            <p>
+                <textarea placeholder="Comente aqui" name="message" class="form-control" ></textarea>
+            </p>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+        <div class="list-group" >
+            
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+
+</script>
